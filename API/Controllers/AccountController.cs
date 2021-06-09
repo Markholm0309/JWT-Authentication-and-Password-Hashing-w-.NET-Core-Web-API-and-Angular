fb79373454eb1e384d923635b19d09821df3bb2a
Mark Holm Justesen <spammailgg25@gmail.com>
2021-06-09 12:16:31 +0200

    Login endpoint fix

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -62,7 +62,14 @@ namespace API.Controllers
 
             if (user == null) return Unauthorized("Invalid username");
 
-            _hashService.CheckHash(user, loginDto.Password);
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+            }
 
             return new UserDto
             {
